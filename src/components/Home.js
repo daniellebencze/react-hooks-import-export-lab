e672ada41b610014d03c5f51c0b4975f65654187
Danielle Bencze <daniellebencze@live.com>
2022-02-01 10:19:56 -0800

    completed assignment

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,10 @@
 import React from "react";
+import {
+  username,
+  city,
+} from "/home/dbencze/react-hooks-import-export-lab/src/data/user.js";
 
-function Home() {
+export default function Home() {
   return (
     <div id="home">
       <h1>
